0b7e89673f722f3a7012a99a7ffa5c7ec21df4be
Miles Rausch <miles@milesrausch.com>
2015-11-03 21:38:12 -0600

    Added Stylelint to the project

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,73 @@ gulp.task('styles', function() {
     var postcss = require('gulp-postcss');
     var autoprefixer = require('autoprefixer');
     var cssnano = require('cssnano');
+    var stylelint = require('stylelint');
+    var reporter = require('postcss-reporter');
 
     return gulp.src('./src/css/*.css')
         .pipe( postcss([
+            stylelint({
+                "rules": {
+                    "string-quotes": [ 2, "double" ],
+                    "color-hex-case": [ 2, "lower" ],
+                    "number-leading-zero": [ 2, "never" ],
+                    "number-no-trailing-zeros": [ 2 ],
+                    "number-zero-length-no-unit": [ 2 ],
+                    "function-comma-space-after": [ 2, "always" ],
+                    "function-comma-space-before": [ 2, "never" ],
+                    "function-parentheses-space-inside": [ 2, "never" ],
+                    "function-space-after": [ 2, "always" ],
+                    "function-url-quotes": [ 2, "double" ],
+                    "value-list-comma-space-after": [ 2, "always" ],
+                    "value-list-comma-space-before": [ 2, "never" ],
+                    "declaration-colon-space-after": [ 2, "always" ],
+                    "declaration-colon-space-before": [ 2, "never" ],
+                    "declaration-block-semicolon-newline-after": [ 2, "always" ],
+                    "declaration-block-semicolon-newline-before": [ 2, "never-multi-line" ],
+                    "declaration-no-important": [ 2 ],
+                    "nesting-block-opening-brace-newline-before": [ 2, "always-multi-line" ],
+                    "nesting-block-opening-brace-space-before": [ 2, "always-multi-line" ],
+                    "block-closing-brace-newline-after": [ 2, "always" ],
+                    "block-closing-brace-newline-before": [ 2, "always" ],
+                    "block-no-empty": [ 2 ],
+                    "block-opening-brace-newline-after": [ 2, "always" ],
+                    "block-opening-brace-space-before": [ 2, "always" ],
+                    "selector-combinator-space-after": [ 2, "always" ],
+                    "selector-combinator-space-before": [ 2, "always" ],
+                    "selector-no-id": [ 2 ],
+                    "selector-list-comma-newline-after": [ 2, "always" ],
+                    "selector-list-comma-newline-before": [ 2, "never-multi-line" ],
+                    "selector-list-comma-space-before": [ 2, "never" ],
+                    "rule-nested-empty-line-before": [ 2, "always", {
+                        except: [ "first-nested" ]
+                    }],
+                    "rule-no-duplicate-properties": [ 2 ],
+                    "rule-no-single-line": [ 2 ],
+                    "rule-non-nested-empty-line-before": [ 2, "always" ],
+                    "rule-properties-order": [ 2, "alphabetical" ],
+                    "rule-trailing-semicolon": [ 2, "always" ],
+                    "root-no-standard-properties": [ 2 ],
+                    "media-feature-colon-space-after": [ 2, "always" ],
+                    "media-feature-colon-space-before": [ 2, "never" ],
+                    "media-feature-range-operator-space-after": [ 2, "always" ],
+                    "media-feature-range-operator-space-before": [ 2, "always" ],
+                    "media-query-parentheses-space-inside": [ 2, "never" ],
+                    "media-query-list-comma-newline-after": [ 2, "never-multi-line" ],
+                    "media-query-list-comma-newline-before": [ 2, "never-multi-line" ],
+                    "media-query-list-comma-space-after": [ 2, "always" ],
+                    "media-query-list-comma-space-before": [ 2, "never" ],
+                    "at-rule-empty-line-before": [ 2, "always" ],
+                    "comment-empty-line-before": [ 2, "always" ],
+                    "comment-space-inside": [ 2, "always" ],
+                    "indentation": [ 2, 4 ],
+                    "no-eol-whitespace": [ 2 ],
+                    "no-missing-eof-newline": [ 2 ],
+                    "no-multiple-empty-lines": [ 2 ]
+                }
+            }),
+            reporter({
+                clearMessages: true
+            }),
             autoprefixer({
                 browsers: [
                     'last 2 versions',
